71eb62e397f2beb0bc92908210110f3bdbd334a7
Tina <bhavleen1995@gmail.com>
2022-05-14 20:42:01 +0530

    fixing play pronunciation

--- frontend/src/components/playPronunciation/PlayPronunciation.js
+++ frontend/src/components/playPronunciation/PlayPronunciation.js
@@ -4,7 +4,7 @@ export const PlayPronunciation = ({data}) => {
     return (
         <div className="play-pronunciation-modal">
             <div className="play-section">
-                <audio src={`/api/v1/pronunciation/byId?uid=${data.uid}&fname=${data.firstname}&lname=${data.lastname}`} controls />
+                <audio src={`/api/v1/pronunciation/byId?uid=${data.uid}&fname=${data.firstname}&lname=${data.lastname}&country=${data.location}`} controls />
             </div>
         </div>
     );
